8c41a8333410b6b9688f634bebf2490a00ed47e6
maharanasarkar <maharanasarkars3@gmail.com>
2025-07-24 23:10:24 +0530

    Added 'httpx'

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as f:
 
 setup(
     name="whatsloon",
-    version="2.0.2",
+    version="2.0.3",
     packages=find_packages(),
     url="https://github.com/maharanasarkar/whatsloon",
     author="Maharana Sarkar",
